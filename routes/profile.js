d6a2c3785f3e1adbc60eefeda0d2b51e33d9bf56
Dmitry Shevelev <igrave1988@gmail.com>
2016-10-06 17:14:48 +0500

    User save

--- routes/profile.js
+++ routes/profile.js
@@ -7,7 +7,6 @@ let express = require('express'),
     router = express.Router();
     
 //router.use(token_check.token_check);
-
 router.get('/', load_user, function(req, res) {
     if(req.user) {
         User.findOne({
@@ -43,5 +42,80 @@ router.get('/', load_user, function(req, res) {
             res.redirect('/login');
         }
 });
+// User settings
+router.get('/settings', load_user, function(req, res) {
+    if(req.user) {
+        User.findOne({
+            token: req.cookies.token
+        }, function (err, user) {
+                if(err) {
+                    throw err;
+                } else {
+                    if (!user){
+                        res.redirect('login');
+                    } else {
+                        Room.find({}, function(err, findedRooms){
+                                if(err) {
+                                    throw err;
+                                } else {
+                                    Company.findOne({'_id': user.company}, function(err, company) {
+                                            if(err) {
+                                                throw err;
+                                            } else {
+                                                res.render('worker-settings', {
+                                                                        user: user,
+                                                                        rooms: findedRooms,
+                                                                        company: company,
+                                                                        isLogin: true,
+                                                                        isSaved: null});   
+                                            }
+                                        });
+                                }
+                            });
+                    };
+                }
+            });
+        } else {
+            res.redirect('/login');
+        }
+});
+router.post('/settings', load_user, function(req, res){
+        if(req.user) {
+            User.findOne({
+                    token: req.cookies.token
+                }, function(err, user) {
+                        if(err) {
+                            
+                            throw err;
+                        } else {
+                            Room.find({}, function(err, findedRooms) {
+                                if(err) {
+                                    throw err;
+                                } else {
+                                    user.name = req.body.name;
+                                    user.surname = req.body.surname;
+                                    user.lastname = req.body.lastname;
+                                    user.save(function(err) {
+                                    if(err) { 
+                                        res.render('worker-settings', {
+                                                                        user: user,
+                                                                        rooms: findedRooms,
+                                                                        isLogin: true,
+                                                                        isSaved: false});
+                                    } else {
+                                        res.render('worker-settings', {
+                                                                        user: user,
+                                                                        rooms: findedRooms,
+                                                                        isLogin: true,
+                                                                        isSaved: true});
+                                    }});
+                                }
+                            });
+                        }
+                });
+        } else {
+            res.redirect('/login');
+        }
+    });
 
 module.exports = router;
