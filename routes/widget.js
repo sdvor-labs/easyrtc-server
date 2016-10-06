2574b2355d08fd3a52a97ac2a1f1a39ef1f0f97a
nkotov <nkotov@sdvor.com>
2016-10-06 08:50:06 +0500

    redirect if room private and user not registered

--- routes/widget.js
+++ routes/widget.js
@@ -1,16 +1,24 @@
 // Load modules
 let express = require('express'),
     router = express.Router(),
-    Room = require('../models/room.js');
+    Room = require('../models/room.js'),
+    load_user = require('../middleware/load_user');
 /* GET about listing. */
-router.get('/:room_name', function(req, res) {
+router.get('/:room_name', load_user, function(req, res) {
     Room.findOne({'name': req.params.room_name}, function(err, findedRoom){
             if(err) {
                 throw err;
             } else {
                 if(findedRoom !== null) {
-                    res.render('widget', { roomLabel: findedRoom.label,
-                                         roomName: req.params.room_name});
+                    if (findedRoom.visiability == 'private')
+                        if (req.user)
+                            res.render('widget', { roomLabel: findedRoom.label,
+                                                 roomName: req.params.room_name});
+                        else
+                            res.redirect('/login');
+                    else
+                        res.render('widget', { roomLabel: findedRoom.label,
+                            roomName: req.params.room_name});
                 } else {
                     res.render('error-room');
                 }        
