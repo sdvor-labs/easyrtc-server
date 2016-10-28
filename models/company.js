c99608ddcefe5684566fdf15f45267b0d676c8f0
Dmitry Shevelev <igrave1988@gmail.com>
2016-10-28 14:00:40 +0500

    small comment

--- models/company.js
+++ models/company.js
@@ -1,6 +1,19 @@
+/**
+ * @author Nikita Kotov
+ * @file company
+ * @description Model for company collection
+ */ 
 let mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
-	
+/**
+ *	Objcet schema for company mogoose object
+ *	@typedef companySchema
+ *	@type {object}
+ *	@property {string} name - name of company
+ *	@property {string} address - location of company
+ *	@property {string} site - url site company
+ *	@property {string} additional_info - additional information about company
+ */
 let companySchema = new Schema({
 	name: String,
 	address: String,
@@ -9,5 +22,8 @@ let companySchema = new Schema({
 });
 
 let Company = mongoose.model('Company', companySchema);
-
+/**
+ * Object Company for work with collection companies
+ * @exports Company
+ */
 module.exports = Company;
